4e2f76d5ea0bee0991c56af7d35a3fe70e4ddf4f
Viki Meng <vmeng004@mac.nam.pwcinternal.com>
2017-09-20 11:43:12 +0800

    first commit

---
+++ routes.js
@@ -0,0 +1,24 @@
+const Router = require('express').Router;
+const router = new Router();
+
+const user = require('./model/user/router');
+const pet = require('./model/pet/router');
+
+// const requestify = require('requestify');
+
+router.route('/').get((req, res) => {
+  res.json({ message: 'Welcome to  API!' });
+});
+
+router.use('/user', user);
+router.use('/pet', pet);
+const requestify = require('requestify');
+router.use('/test', function(req,res,next){
+	// res.send('ok');
+	requestify.get("http://localhost:4000").then(r => {
+			console.log(r)
+			res.send(r)
+		})
+
+})
+module.exports = router;
