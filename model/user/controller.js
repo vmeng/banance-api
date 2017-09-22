062965a502dccd8c63558eaa747b2a647d1dd146
Viki Meng <vmeng004@mac.nam.pwcinternal.com>
2017-09-22 15:14:47 +0800

    creat user api

--- model/user/controller.js
+++ model/user/controller.js
@@ -8,44 +8,19 @@ axios.defaults.baseURL = 'http://localhost:4000/'
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 class UserController extends Controller {
-	// create(req, res, next) {
-	// 	// var doc = super.create(req, res, next);
-	// 	// console.log(doc)
-	// 	var doc
-	// 	// requestify.get("https://www.npmjs.com/package/axios").then(r => {
-	// 	// 	console.log(r)
-	// 	// 	res.send(r)
-	// 	// })
-
-	// 	requestify.get("http://localhost:8081").then(r => {
-	// 		console.log(r)
-	// 		res.send(r)
-	// 	})
-
-
-	// 	requestify.request('http://localhost:8081',{
-	// 		method: 'GET',
-	// 		// body: {username:'req.body.username', orgName: 'org1'},
-	// 		// headers:{
-	// 		// 	'Content-Type': 'application/json'
-	// 		// }
-	// 	})
-	// 	.then(function(result) {
-	// 		console.log(result);
-	// 		// res.status(201).json( result)
-	// 	})
-		// super.facade.create(req.body).then(_doc => {
-  // 			doc = _doc;
-  // 			console.log(doc)
-		//   	return axios.post('users',{username:req.body.username, orgName: req.body.orgName})
-		//   	// return requestify.post('http://localhost:4000/users',{username:req.body.username, orgName: req.body.orgName})
-		// }).then(result => {
-	 //  		console.log(result)
-	 //  		res.status(201).json(Object.assign(doc, result))
-	 //  		// res.status(201).json(Object.assign(doc))
-	 //  	}).catch(err => next(err));
-
-	// }
+	create(req, res, next) {
+		var doc;
+		super.create(req, res, next)
+		.then(_doc => {
+			doc = _doc;
+			return axios.post('users', {username:req.body.username, orgName: req.body.orgName})
+		}).then(result => {
+			// console.log('----')
+	  // 		console.log(result.data)
+	  // 		console.log('----')
+	  		res.json(Object.assign({}, doc, result.data))
+	  		// res.status(201).json(Object.assign(doc))
+	  	}).catch(err => next(err));
+	}
 }
-
 module.exports = new UserController(userFacade);
